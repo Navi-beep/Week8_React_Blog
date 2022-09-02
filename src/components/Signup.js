eff0b49403b8c831e8acf7d37d040d60a5c19d7d
Navi-Beep <estawars30@gmail.com>
2022-09-02 17:46:33 -0500

    reated signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -17,10 +17,24 @@ import { useNavigate } from 'react-router-dom'
 
                 <>
                     <h4 className="text-center"> Sign Up Here!!</h4>
+                    <form>
+                        <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type='text' className='form-control' placeholder='Enter Email' name ='email' />
 
+                        <label htmlFor="Username">Username</label>
+                        <input type='text' className='form-control' placeholder='Enter Username' name ='username' />
 
+                        <label htmlFor="password">Password</label>
+                        <input type='text' className='form-control' placeholder='Enter Password' name ='password' />
 
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input type='text' className='form-control' placeholder=' Please Re-enter Password' name ='confirmPassword' />
 
+                        <input type='submit' className='btn btn-dark w-75 mt-3' value='Sign Up' />
+                        
+                        </div>
+                    </form>
                 </>
             )
 
